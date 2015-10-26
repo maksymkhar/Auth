c87b51e8d1bc104d13121caf08afb483a4603d07
Max <maksymkharuk@iesebre.com>
2015-10-26 17:41:59 +0100

    Added loguin form

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,6 +20,10 @@ Route::get('/home', function () {
     return view('home');
 });
 
+Route::get('/login', function () {
+    return view('login');
+});
+
 Route::get('/resource', function () {
 
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,11 +2,60 @@
 <html>
 <head>
     <title>Login</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+    <style>
+        html, body {
+            height: 100%;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 96px;
+        }
+    </style>
 </head>
 <body>
-<div >
-    <div >
+<div class="container">
+    <div class="content">
         <div class="title">LOGIN</div>
+
+        <form>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email">
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <button type="submit" class="btn btn-default">Login</button>
+            <button type="reset" class="btn btn-default">Reset</button>
+        </form>
+
+
+
     </div>
 </div>
 </body>
